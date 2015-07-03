c2266d0be31e49e5112bb2a33223954a49e5ee90
Gleb Kozyrev <gleb@gkoz.com>
2015-07-03 11:13:04 +0300

    Remove the raw method and use ToGlibPtr directly
    Let the type system do its work, don't cast pointer types pointlessly.

--- src/secret_collection.rs
+++ src/secret_collection.rs
@@ -45,46 +45,46 @@ impl SecretCollection {
     /// Get the created date and time of the collection.
     /// The return value is the number of seconds since the unix epoch, January 1st 1970.
     pub fn get_created(&self) -> u64 {
-        unsafe {ffi::secret_collection_get_created(self.raw())}
+        unsafe {ffi::secret_collection_get_created(self.to_glib_none().0)}
     }
 
     /// Get the modified date and time of the collection.
     /// The return value is the number of seconds since the unix epoch, January 1st 1970.
     pub fn get_modified(&self) -> u64 {
-        unsafe {ffi::secret_collection_get_modified(self.raw())}
+        unsafe {ffi::secret_collection_get_modified(self.to_glib_none().0)}
     }
 
     /// Get the Secret Service object that this collection was created with.
     pub fn get_service(&self) -> SecretService { //TODO find out if this can return null
         unsafe {
-            let ptr = ffi::secret_collection_get_service(self.raw());
+            let ptr = ffi::secret_collection_get_service(self.to_glib_none().0);
             SecretService::wrap(Ref::from_glib_none(ptr as *mut GObject))
         }
     }
 
     pub fn are_items_loaded(&self) -> bool {
-        let flags = unsafe {ffi::secret_collection_get_flags(self.raw())};
+        let flags = unsafe {ffi::secret_collection_get_flags(self.to_glib_none().0)};
         flags & SECRET_COLLECTION_LOAD_ITEMS != 0
     }
 
     /// Get the label of this collection.
     pub fn get_label(&self) -> String {
         unsafe{
-            let ptr = ffi::secret_collection_get_label(self.raw());
+            let ptr = ffi::secret_collection_get_label(self.to_glib_none().0);
             FromGlibPtr::from_glib_none(ptr)
         }
     }
 
     /// Get whether the collection is locked or not.
     pub fn get_locked(&self) -> bool {
-        let gbool = unsafe{ffi::secret_collection_get_locked(self.raw())};
+        let gbool = unsafe{ffi::secret_collection_get_locked(self.to_glib_none().0)};
         FromGlib::from_glib(gbool)
     }
 
     /// Returns None, if the items have not yet been loaded.
     pub fn get_items(&self) -> Option<Vec<SecretItem>> {
         unsafe {
-            let glist = ffi::secret_collection_get_items(self.raw());
+            let glist = ffi::secret_collection_get_items(self.to_glib_none().0);
             if glist.is_null(){
                 None
             } else {
@@ -94,17 +94,11 @@ impl SecretCollection {
 
     }
 
-
-    #[inline]
-    fn raw(&self) -> *mut ffi::SecretCollectionFFI {
-        self.0.to_glib_none() as *mut ffi::SecretCollectionFFI
-    }
-
     /// Ensure that the SecretCollection proxy has loaded all the items present in the Secret Service.
     pub fn load_items(&self) -> SecretResult<()>{
         unsafe {
             let mut err = ptr::null_mut();
-            ffi::secret_collection_load_items_sync(self.raw(), ptr::null_mut(), &mut err);
+            ffi::secret_collection_load_items_sync(self.to_glib_none().0, ptr::null_mut(), &mut err);
             if err.is_null() {
                 Ok(())
             } else {
