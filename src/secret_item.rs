06b63b7292ff0396461f8706a203a60a148d3942
Nicolas Koch <nioko1337@gmail.com>
2015-07-15 14:04:58 +0200

    Implement several new methods for SecretItem

--- src/secret_item.rs
+++ src/secret_item.rs
@@ -6,18 +6,36 @@ use glib::object::{Wrapper, Ref, Object, Upcast};
 use glib::types::{StaticType, Type};
 use glib::translate::*;
 use secret_service::SecretService;
+use secret_collection::SecretCollection;
 use secret_value::SecretValue;
 use SecretResult;
 use ffi;
 
 /// SecretItem represents a secret item stored in the Secret Service.
-/// Each item has a value, represented by a SecretValue, which can be retrieved by `get_secret()` or set by `set_secret()` (TODO). The item is only available when the item is not locked.
+/// Each item has a value, represented by a SecretValue, which can be retrieved by `get_secret()` or set by `set_secret()`. The item is only available when the item is not locked.
 /// Items can be locked or unlocked using the `Lock::lock()` or `Lock::unlock()` functions. The Lock trait is implemented by SecretItem. The Secret Service may not be able to unlock individual items, and may unlock an entire collection when a single item is unlocked.
-/// Each item has a set of attributes, which are used to locate the item later. These are not stored or transferred in a secure manner. Each attribute has a string name and a string value. Use `SecretService::search()` to search for items based on their attributes, and `set_attributes()` (TODO) to change the attributes associated with an item.
-/// Items can be created with `create()` (TODO) or `SecretService::store()`.
+/// Each item has a set of attributes, which are used to locate the item later. These are not stored or transferred in a secure manner. Each attribute has a string name and a string value. Use `SecretService::search()` to search for items based on their attributes, and `set_attributes()` to change the attributes associated with an item.
+/// Items can be created with `create()` or `SecretService::store()`.
 pub struct SecretItem(Ref);
 
 impl SecretItem {
+
+    /// Create a new item in the secret service.
+    /// collection: a secret collection to create this item in
+    /// attributes: attributes for the new item.
+    /// label: label for the new item
+    /// value: secret value for the new item
+    pub fn create(collection: &SecretCollection, attributes: &HashMap<String, String>, label: &str, value: &SecretValue) -> SecretResult<SecretItem> {
+        let mut err = ptr::null_mut();
+        unsafe {
+            let item = ffi::secret_item_create_sync(collection.to_glib_none().0, ptr::null(), attributes.to_glib_none().0, label.to_glib_none().0, value.to_glib_none(), SECRET_ITEM_CREATE_NONE, ptr::null_mut(), &mut err);
+            if err.is_null() { //TODO for all patterns like this: This if does not need to be in the unsafe block. Fix pls.
+                Ok(from_glib_full(item))
+            } else {
+                Err(Error::wrap(err))
+            }
+        }
+    }
 
     /// Delete this secret item.
     pub fn delete(&self) -> SecretResult<()> {
@@ -89,11 +107,38 @@ impl SecretItem {
         }
     }
 
+    /// Set the secret value of this item.
+    /// Each item has a single secret which might be a password or some other secret binary value (not supported yet).
+    pub fn set_secret(&self, value: &SecretValue) -> SecretResult<()> {
+        let mut err = ptr::null_mut();
+        unsafe {
+            ffi::secret_item_set_secret_sync(self.to_glib_none().0, value.to_glib_none(), ptr::null_mut(), &mut err);
+            if err.is_null() {
+                Ok(())
+            } else {
+                Err(Error::wrap(err))
+            }
+        }
+    }
+
     /// Get the attributes of this item.
     pub fn get_attributes(&self) -> HashMap<String, String> {
         unsafe {
             let ptr = ffi::secret_item_get_attributes(self.to_glib_none().0);
             HashMap::from_glib_full(ptr)
+        }
+    }
+
+    /// Set the attributes of this item.
+    pub fn set_attributes(&self, attributes: &HashMap<String, String>) -> SecretResult<()> {
+        let mut err = ptr::null_mut();
+        unsafe {
+            ffi::secret_item_set_attributes_sync(self.to_glib_none().0, ptr::null(), attributes.to_glib_none().0, ptr::null_mut(), &mut err);
+            if err.is_null() {
+                Ok(())
+            } else {
+                Err(Error::wrap(err))
+            }
         }
     }
 }
@@ -120,3 +165,7 @@ impl Wrapper for SecretItem {
         self.0
     }
 }
+
+const SECRET_ITEM_CREATE_NONE: i32        = 0;
+#[allow(dead_code)]
+const SECRET_ITEM_CREATE_REPLACE: i32     = 1 << 1;
