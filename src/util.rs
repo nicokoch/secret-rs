3bd0a1f67b4f35f882c89212a21d0594a01e79f3
Loren M. Lang <lorenl@north-winds.org>
2023-09-09 20:38:01 -0700

    FIXME: Revert this workaround for compilation errors
    This comments out lines that breaks the lock/unlock calls for secret
    items and collections. This will need to be reverted once the correct
    code has been implemented to get this to compile on glib-rs.
    
    With this workaround added, the crate compiled even with current stable
    and nightly compilers using this older glib 0.3.1 crate.

--- src/util.rs
+++ src/util.rs
@@ -3,31 +3,35 @@ use glib::Error;
 use glib_sys::{GList};
 use glib::wrapper::{Wrapper};
 use glib::translate::{ToGlibPtr, FromGlibPtrContainer, GlibPtrDefault};
+use glib::translate::ToGlibContainerFromSlice;
 use glib::types::StaticType;
 use secret_item::SecretItem;
 use secret_collection::SecretCollection;
 use ffi;
 use SecretResult;
 
-pub fn lock_object<W: Wrapper + StaticType + GlibPtrDefault>(obj: &W) -> SecretResult<Vec<W>>{
+//pub fn lock_object<'a, W: Wrapper + StaticType + GlibPtrDefault + ToGlibContainerFromSlice<'a, *mut <W as GlibPtrDefault>::GlibType>>(obj: &W) -> SecretResult<Vec<W>>{
+//pub fn lock_object<'a, W: Wrapper + StaticType + GlibPtrDefault + ToGlibContainerFromSlice<'a, *mut GList>>(obj: &W) -> SecretResult<Vec<W>>{
+pub fn lock_object<'a, W: Wrapper + StaticType + GlibPtrDefault + ToGlibPtr<'a, <W as GlibPtrDefault>::GlibType>>(obj: &'a W) -> SecretResult<Vec<W>>{
     debug_assert!(W::static_type() == SecretItem::static_type() || W::static_type() == SecretCollection::static_type(), "Can only lock items or collections");
     let mut err = ptr::null_mut();
     let mut res = ptr::null_mut();
     let arr = [obj];
-    let slice = (&arr[..]).to_glib_none();
+    //let slice: (*mut GList, <&W as ToGlibContainerFromSlice<'a, *mut GList>>::Storage) = ToGlibContainerFromSlice::to_glib_none_from_slice(&arr[..]);
     unsafe {
         ffi::secret_service_lock_sync(
             ptr::null_mut(),
-            slice.0 as *mut GList,
+            //slice.0 as *mut GList,
+            ptr::null_mut(),
             ptr::null_mut(),
             &mut res,
             &mut err
             );
-        if err.is_null() {
-            Ok(Vec::from_glib_full(res))
-        } else {
+        //if err.is_null() {
+        //    Ok(Vec::from_glib_full(res))
+        //} else {
             Err(Error::wrap(err))
-        }
+        //}
     }
 }
 
@@ -36,19 +40,20 @@ pub fn unlock_object<W: Wrapper + StaticType + GlibPtrDefault>(obj: &W) -> Secre
     let mut err = ptr::null_mut();
     let mut res = ptr::null_mut();
     let arr = [obj];
-    let slice = (&arr[..]).to_glib_none();
+    //let slice = (&arr[..]).to_glib_none();
     unsafe {
         ffi::secret_service_unlock_sync(
             ptr::null_mut(),
-            slice.0 as *mut GList,
+            //slice.0 as *mut GList,
+            ptr::null_mut(),
             ptr::null_mut(),
             &mut res,
             &mut err
             );
-        if err.is_null() {
-            Ok(Vec::from_glib_full(res))
-        } else {
+        //if err.is_null() {
+        //    Ok(Vec::from_glib_full(res))
+        //} else {
             Err(Error::wrap(err))
-        }
+        //}
     }
 }
